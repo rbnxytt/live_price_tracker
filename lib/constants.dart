495287a7c00cd1074db9cc6d99b82a1b562f23d3
Robin <rdalumpines@umindanao.edu.ph>
2022-08-13 20:05:11 +0800

    improved UI, add price feature

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+var tableHeaderStyle =
+    const TextStyle(fontSize: 28.0, fontWeight: FontWeight.w600);
+
+var marketTextStyle =
+    const TextStyle(fontSize: 25.0, fontWeight: FontWeight.w600);
+var defaultBackground = const Color(0xff080820);
+
+double priceFontSize = 20.0;
